f63a1decad71232d71c9c2aec735477d0e3ee48e
Joe Hachem <joehachem2000@gmail.com>
2021-04-22 23:34:14 +0300

    header fix

--- src/components/UI/Buttons/Button/Button.js
+++ src/components/UI/Buttons/Button/Button.js
@@ -2,7 +2,15 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => (
-    <button className={classes.Button} onClick={props.clicked} disabled={props.disabled}>{props.children}</button>
+  <button
+    className={[classes.Button, props.className ? props.className : null].join(
+      ' ',
+    )}
+    onClick={props.clicked}
+    disabled={props.disabled}
+  >
+    {props.children}
+  </button>
 );
 
 export default Button;
